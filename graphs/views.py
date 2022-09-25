ebdf66c2028221dd351825256f4409ed87a45372
blukky <ya.gud@mail.ru>
2022-09-25 13:20:36 +0300

    backend

--- graphs/views.py
+++ graphs/views.py
@@ -1,54 +1,121 @@
 from django.shortcuts import render
-# from django.http import HttpResponse, JsonResponse
-# import tensorflow as tf
-# from keras.models import load_model
-# import neurokit2 as nk
-# import numpy as np
-# import pandas as pd
-# from scipy.signal import find_peaks
-# import os
-# import json
+import pandas as pd
+import numpy as np
+import re
 
-# def get_data_from_file(df):
-#     df.columns = [i.lower() for i in df.columns]
-#     X1 = list()
-#     X2 = list()
-#     X3 = list()
-#     ID = list()
-#     for user, data_user in df.groupby("filename"):
-#         for test, test_data in data_user.groupby(["test_index"]):
-#             for repert, repert_data in test_data.groupby(["presentation"]):
-#                 X_1 = list()
-#                 X_2 = list()
-#                 X_3 = list()
-#                 for ind, question in enumerate(
-#                         list(repert_data.question)):
-#                     data = repert_data[repert_data["question"] == question]
-#                     data_1 = np.array([int(i) * (-1) for i in list(data.data)[0][1:-1].split(", ")])
-#                     data_2 = np.array([int(i) * (-1) for i in list(data.data_2)[0][1:-1].split(", ")])
-#                     signals, info = nk.ppg_process(data_1, sampling_rate=20)
-#                     X_1.append(data_1)
-#                     X_2.append(data_2)
-#                     X_3.append(np.array(list(signals.PPG_Rate)))
-#                     ID.append([user, str(test), str(repert), str(question)])
-#                 if len(X_1) == 0:
-#                     continue
-#                 X_1 = np.array(X_1).reshape((len(X_1) * 240,))
-#                 X_2 = np.array(X_1).reshape((len(X_2) * 240,))
-#                 X_3 = np.array(X_2).reshape((len(X_3) * 240,))
-#                 X_1 = (X_1 - X_1.min()) / (X_1.max() - X_1.min())
-#                 X_3 = (X_3 - X_3.min()) / (X_3.max() - X_3.min())
-#                 X_2 = (X_2 - X_2.min()) / (X_2.max() - X_2.min())
-#                 X1 += [X_1[i * 240:240 * (i + 1)] for i in range(X_1.shape[0] // 240)]
-#                 X3 += [X_3[i * 240:240 * (i + 1)] for i in range(X_3.shape[0] // 240)]
-#                 X2 += [X_2[i * 240:240 * (i + 1)] for i in range(X_2.shape[0] // 240)]
-#     X1 = np.array(X1)
-#     X2 = np.array(X2)
-#     X3 = np.array(X3)
-#     return X1, X2, X3, ID
+
+def find_table(x):
+    x = re.split(",| ", x)
+    arr = list()
+    for i in x:
+        if i.startswith("tbl"):
+            arr.append(i)
+    return arr
+
+
+def type_query(x):
+    x = x.split(",")
+    arr = list()
+    for i in x:
+        ret = i.split(" ")[0]
+        if ret.lower() == "into":
+            arr.append(0)
+        else:
+            arr.append(1)
+    return arr
+
+
+def find_relevant(name):
+    df = pd.read_csv("templates/static/" + name)
+    df["type_user"] = df.loguser.apply(lambda x: 1 if x.startswith("etl") else 0)
+    """
+    Создание столбца table с массивом всех таблиц в запросе
+    """
+    df["table"] = df["query"].apply(find_table)
+    """
+    Создания столбца type_query с массивом типов запросов * 1- from, join ; 0 - into*
+    """
+    df["type_query"] = df["query"].apply(type_query)
+    df.drop(columns=["rn", "query"], inplace=True)
+    """
+    Создане словаря с ключом - название таблицы;
+                      заначение - масив 1 элемент - количестово выгрузок пользователями
+                                        2 элемент - количество выгрузок программистами
+                                        3 элемент - количество загрузок пользователем
+                                        4 элемент - количество загрузок программистом
+                                        5 элемент - количество выгрузок
+                                        6 элемент - количество загрузок
+    """
+    dt = dict()
+    for row in range(len(df)):
+        for ind_table in range(len(df.iloc[row, 2])):
+            table = df.iloc[row, 2][ind_table]
+            if not table in dt:
+                dt[table] = [0] * 6
+            if df.iloc[row, 3][ind_table] == 0:
+                dt[table][4] += 1
+                if df.iloc[row, 1] == 1:
+                    dt[table][0] += 1
+                else:
+                    dt[table][1] += 1
+            elif df.iloc[row, 3][ind_table] == 1:
+                dt[table][5] += 1
+                if df.iloc[row, 1] == 1:
+                    dt[table][2] += 1
+                else:
+                    dt[table][3] += 1
+    table = list(dt.keys())
+    count_down = list()
+    count_select = list()
+    count_down_etl = list()
+    count_select_etl = list()
+    count_down_dev = list()
+    count_select_dev = list()
+    for tb in dt:
+        count_down.append(dt[tb][4])
+        count_select.append(dt[tb][5])
+        count_down_etl.append(dt[tb][0])
+        count_select_etl.append(dt[tb][2])
+        count_down_dev.append(dt[tb][1])
+        count_select_dev.append(dt[tb][3])
+    new_data = pd.DataFrame({"table": table,
+                             "count_all_into": count_down,
+                             "count_all_select": count_select,
+                             "count_etl_into": count_down_etl,
+                             "count_etl_select": count_select_etl,
+                             "count_dev_into": count_down_dev,
+                             "count_dev_select": count_select_dev})
+    new_data[["count_all_into", "count_etl_into"]] = new_data[["count_all_into", "count_etl_into"]].apply(
+        lambda x: x + 1 if x.count_etl_into == 0 else x, axis=1)
+    q = new_data.count_dev_select.sum()
+    qq = new_data.count_etl_into.sum()
+    new_data["conv_etl"] = new_data.count_etl_into / qq
+    new_data["conv_dev"] = new_data.count_dev_select / q
+    new_data["relevant"] = new_data.conv_dev / new_data.conv_etl
+    new_data = new_data.fillna(0)
+    data = new_data.sort_values(by='relevant', ascending=False)
+    new_csv = data[data["relevant"] != 0]
+    new_csv["table"].to_csv("templates/static/" + name.split(".")[:-1] + "_new.csv")
+    return data[["table", "count_etl_into", "count_dev_select", "conv_etl", "conv_dev", "relevant"]].iloc[
+           :100].tolist(), data[["table", "count_etl_into", "count_dev_select", "conv_etl", "conv_dev",
+                                 "relevant"]].iloc[-100:].tolist(), "templates/static/" + name.split(".")[
+                                                                                          :-1] + "_new.csv", name.split(
+        ".")[:-1] + "_new.csv"
+
+
+def save_file(f):
+    with open(f'templates/static/{f.name}', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
 
 def index(request):
-    return render(request, 'index.html', )
+    if request.POST:
+        save_file(request.FILES.get("file"))
+        table1, table2, new_csv, name = find_relevant(request.FILES.get("file").name)
+        return render(request, 'index.html', {"table1": table1, "table2": table2, "new_file": new_csv, "name": name})
+    else:
+        return render(request, 'index.html')
 
 
 def api(request):
